790919354b20fb3235663b4c79c2c34c98554ea4
Kayanna Chandler <kayanna.chandler@virginpulse.com>
2021-11-06 20:01:08 -0500

    initializes socket.io

--- api/server.js
+++ api/server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const cors = require('cors');
 const server = express();
-server.use(express.json());
+server.use( express.json() );
 
 server.use(cors({
-    origin: ['http://localhost:63342', 'https://howto-frontend.netlify.com'],
+    origin: ['http://localhost:3000', 'https://howto-frontend.netlify.com'],
 }));
 
 /* routes */
@@ -17,4 +17,19 @@ server.use( '/ping', ping );
 server.use('/api/products', products);
 server.use('/api/product-reviews', productReviews);
 
+/* websocket */
+
+const http = require('http').Server(server);
+const io = require('socket.io')(http);
+
+io.on("connection", (socket) => {
+    console.log("New client connected");
+    socket.emit("welcome", "Hello and Welcome to the Server");
+})
+
+http.listen(4000, function() {
+    console.log('listening on *:4000');
+});
+
 module.exports = server;
+
